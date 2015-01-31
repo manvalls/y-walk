7ea9d9b9f484e5865b000f043afd5c6e800005af
Manuel Valls Fernández <manolo@vlrz.es>
2015-01-31 17:29:23 +0100

    Added array support

--- main/proto.js
+++ main/proto.js
@@ -1,11 +1,13 @@
 var Resolver = require('y-resolver'),
     Su = require('u-su'),
-    
     walk = require('../main.js'),
     
     yielded = Su(),
     
+    walkArray,
     toYielded = walk.toYielded;
+
+// Promise
 
 if(global.Promise) Su.define(Promise.prototype,toYielded,function(){
   var resolver;
@@ -19,4 +21,17 @@ if(global.Promise) Su.define(Promise.prototype,toYielded,function(){
   return this[yielded] = resolver.yielded;
 });
 
+// Array (Promise.all equivalent)
+
+walkArray = walk.wrap(function*(array){
+  var result = [],element;
+  
+  while(element = array.shift()) result.push(yield element);
+  
+  return result;
+});
+
+Su.define(Array.prototype,toYielded,function(){
+  return this[yielded] = this[yielded] || walkArray(this);
+});
 
