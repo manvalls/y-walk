46fded2009cc28ed3394728ddcee42e5e541eab9
Manuel Valls Fernández <manolo@vlrz.es>
2015-02-01 10:14:50 +0100

    Added Object support

--- main/proto.js
+++ main/proto.js
@@ -4,7 +4,7 @@ var Resolver = require('y-resolver'),
     
     yielded = Su(),
     
-    walkArray,
+    race,
     toYielded = walk.toYielded;
 
 // Promise
@@ -33,3 +33,31 @@ Su.define(Array.prototype,toYielded,walk.wrap(function*(){
   return result;
 }));
 
+// Object (Promise.race equivalent)
+
+race = walk.wrap(function*(ctx,key,yd){
+  
+  yield ctx.ready;
+  
+  try{ ctx.resolver.accept([key,yield yd]); }
+  catch(e){
+    if(!--ctx.toFail) ctx.resolver.reject(e);
+  }
+  
+});
+
+Su.define(Object.prototype,toYielded,function(){
+  var ready = new Resolver(),
+      keys = Object.keys(this),
+      ctx = {
+        ready: ready.yielded,
+        resolver: new Resolver(),
+        toFail: keys.length
+      },
+      i;
+  
+  for(i = 0;i < keys.length;i++) race(ctx,keys[i],this[keys[i]]);
+  ready.accept();
+  
+  return ctx.resolver.yielded;
+});
